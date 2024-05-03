84c09d4bf08f4da0322b2d05e869f3313e0186ee
Nathan Wang <nathan.r.wang@gmail.com>
2024-05-02 22:05:51 -0400

    add more constant folding operations

--- src/opt/constant_folding.rs
+++ src/opt/constant_folding.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{backtrace, collections::HashMap};
 
 use crate::inter::{
     constant::Const,
@@ -11,14 +11,13 @@ pub fn fold_constants(method: &mut Method) {
 
     for block in dom.preorder(method.entry) {
         for inst in method.block(block).insts.clone() {
+            let mut const_result = None;
             match method.inst_mut(inst) {
                 Inst::LoadConst(value) => {
-                    constant_instructions.insert(inst, value.clone());
+                    const_result = Some(value.clone());
                 }
                 Inst::Copy(copied_inst) => {
-                    if let Some(val) = constant_instructions.get(copied_inst) {
-                        constant_instructions.insert(inst, val.clone());
-                    }
+                    const_result = constant_instructions.get(copied_inst).cloned();
                 }
                 Inst::Add(a, b)
                 | Inst::Sub(a, b)
@@ -29,22 +28,68 @@ pub fn fold_constants(method: &mut Method) {
                         (constant_instructions.get(a), constant_instructions.get(b))
                     {
                         if let (Const::Int(a), Const::Int(b)) = (a, b) {
-                            let result = match method.inst(inst) {
+                            const_result = Some(Const::Int(match method.inst(inst) {
                                 Inst::Add(..) => a + b,
                                 Inst::Sub(..) => a - b,
                                 Inst::Mul(..) => a * b,
                                 Inst::Div(..) => a / b,
                                 Inst::Mod(..) => a % b,
                                 _ => unreachable!(),
-                            };
-                            *method.inst_mut(inst) = Inst::LoadConst(Const::Int(result));
-                            constant_instructions.insert(inst, Const::Int(result));
+                            }));
                         } else {
-                            panic!("expected add/sub/mul/div/mod instruction arguments to both be integers");
+                            unreachable!()
                         }
                     }
                 }
+                Inst::Not(a) => {
+                    if let Some(a) = constant_instructions.get(a) {
+                        if let Const::Bool(a) = a {
+                            const_result = Some(Const::Bool(!a));
+                        } else {
+                            unreachable!()
+                        }
+                    }
+                }
+                Inst::Neg(a) => {
+                    if let Some(a) = constant_instructions.get(a) {
+                        if let Const::Int(a) = a {
+                            const_result = Some(Const::Int(-a));
+                        } else {
+                            unreachable!()
+                        }
+                    }
+                }
+                Inst::Less(a, b) | Inst::LessEq(a, b) => {
+                    if let (Some(a), Some(b)) =
+                        (constant_instructions.get(a), constant_instructions.get(b))
+                    {
+                        if let (Const::Int(a), Const::Int(b)) = (a, b) {
+                            const_result = Some(Const::Bool(match method.inst(inst) {
+                                Inst::Less(..) => a < b,
+                                Inst::LessEq(..) => a <= b,
+                                _ => unreachable!(),
+                            }));
+                        } else {
+                            unreachable!()
+                        }
+                    }
+                }
+                Inst::Eq(a, b) | Inst::Neq(a, b) => {
+                    if let (Some(a), Some(b)) =
+                        (constant_instructions.get(a), constant_instructions.get(b))
+                    {
+                        const_result = Some(Const::Bool(match method.inst(inst) {
+                            Inst::Eq(..) => a == b,
+                            Inst::Neq(..) => a == b,
+                            _ => unreachable!(),
+                        }))
+                    }
+                }
                 _ => {}
+            }
+            if let Some(result) = const_result {
+                *method.inst_mut(inst) = Inst::LoadConst(result.clone());
+                constant_instructions.insert(inst, result);
             }
         }
     }
