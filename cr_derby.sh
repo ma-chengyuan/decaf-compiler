45dc03fd54e3b8c6bbcd35684d5e2c4fd6805759
Siyong Huang <huangsy222@gmail.com>
2024-05-05 23:56:51 -0400

    Fixed a bug (with kill set)

---
+++ cr_derby.sh
@@ -0,0 +1,4 @@
+RUST_BACKTRACE=1 ./compile_and_run.sh -O all ../public-tests/derby/input/saman_sl.dcf
+# RUST_BACKTRACE=1 ./compile_and_run.sh -O all ../public-tests/derby/input/saman_sl.dcf
+# ./compile_and_run.sh $ARGS ../public-tests/derby/input/noise_median.dcf
+# ./compile_and_run.sh $ARGS ../public-tests/derby/input/segovia_blur.dcf
