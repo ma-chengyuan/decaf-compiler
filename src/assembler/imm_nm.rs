b4fbee4ef295d2858e3c5542c2edb6e0602a3d20
jacobtpl <jacobtpl@gmail.com>
2024-05-14 00:24:16 +0000

    fix merge conflict

--- src/assembler/imm_nm.rs
+++ src/assembler/imm_nm.rs
@@ -57,7 +57,6 @@ impl<'a> ImmediateNonMaterializer<'a> {
                 Inst::Add(lhs, rhs)
                 | Inst::Sub(lhs, rhs)
                 | Inst::Mul(lhs, rhs)
-                | Inst::Div(lhs, rhs)
                 | Inst::Mod(lhs, rhs)
                 | Inst::Eq(lhs, rhs)
                 | Inst::Neq(lhs, rhs)
